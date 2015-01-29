6d0031ac7024383c4050904c2f76f1c8c05d7998
Sean Schwartz <sean.schwartz@servicemesh.com>
2015-01-28 19:36:58 -0600

    Refactored ItemAlert code. Should be more efficient/"faster" as we stop loading information on the item once we determine everything we need from it.

--- src/Hud/Loot/AlertDrawStyle.cs
+++ src/Hud/Loot/AlertDrawStyle.cs
@@ -16,29 +16,29 @@ namespace PoeHUD.Hud.Loot
             { ItemRarity.Unique, HudSkin.UniqueColor },
         };
 
-        public AlertDrawStyle(ItemRarity rarity, bool isSkillGem, bool isCurrency)
+        public AlertDrawStyle(Color color, ItemRarity rarity, int frameWidth, string text, int iconIndex)
         {
-            if (isSkillGem)
+            FrameWidth = frameWidth;
+            Text = text;
+            IconIndex = iconIndex;
+
+            if (color != Color.Black) // Checking against default of black as Color is a struct and I doubt anyone would use this color
             {
-                Color = HudSkin.SkillGemColor;
-            }
-            else if (isCurrency)
-            {
-                Color = HudSkin.CurrencyColor;
+                AlertColor = color;
             }
             else
             {
-                Color color;
-                Color = colors.TryGetValue(rarity, out color) ? color : Color.White;
+                Color tempColor;
+                AlertColor = colors.TryGetValue(rarity, out tempColor) ? tempColor : Color.White;
             }
         }
 
-        public Color Color { get; private set; }
+        public Color AlertColor { get; private set; }
 
-        public int FrameWidth { get; set; }
+        public int FrameWidth { get; private set; }
 
-        public string Text { get; set; }
+        public string Text { get; private set; }
 
-        public int IconIndex { get; set; }
+        public int IconIndex { get; private set; }
     }
 }
--- src/Hud/Loot/ItemAlertPlugin.cs
+++ src/Hud/Loot/ItemAlertPlugin.cs
@@ -84,21 +84,17 @@ namespace PoeHUD.Hud.Loot
 
         protected override void OnEntityAdded(EntityWrapper entity)
         {
-            if (!Settings.Enable || currentAlerts.ContainsKey(entity))
-            {
-                return;
-            }
-            if (entity.HasComponent<WorldItem>())
+            if (Settings.Enable && !currentAlerts.ContainsKey(entity) && entity.HasComponent<WorldItem>())
             {
                 IEntity item = entity.GetComponent<WorldItem>().ItemEntity;
-                ItemUsefulProperties props = EvaluateItem(item);
+                // Check if we should alert WHILE loading item to preven useless compute
+                ItemUsefulProperties props = initItem(item);
 
-                if (props.IsWorthAlertingPlayer(currencyNames, Settings))
+                if (props.ShouldAlert(currencyNames, Settings))
                 {
                     AlertDrawStyle drawStyle = props.GetDrawStyle();
                     currentAlerts.Add(entity, drawStyle);
-                    CurrentIcons[entity] = new MapIcon(entity, new HudTexture("minimap_default_icon.png", drawStyle.Color),
-                        () => Settings.ShowItemOnMap, 8);
+                    CurrentIcons[entity] = new MapIcon(entity, new HudTexture("minimap_default_icon.png", drawStyle.AlertColor), () => Settings.ShowItemOnMap, 8);
 
                     if (Settings.PlaySound && !playedSoundsCache.Contains(entity.LongId))
                     {
@@ -231,7 +227,7 @@ namespace PoeHUD.Hud.Loot
             double distance = delta.GetPolarCoordinates(out phi);
             float compassOffset = Settings.TextSize + 8;
             Vector2 textPos = position.Translate(-padding.X - compassOffset, padding.Y);
-            Size2 textSize = Graphics.DrawText(text, Settings.TextSize, textPos, drawStyle.Color, FontDrawFlags.Right);
+            Size2 textSize = Graphics.DrawText(text, Settings.TextSize, textPos, drawStyle.AlertColor, FontDrawFlags.Right);
             int iconSize = drawStyle.IconIndex >= 0 ? textSize.Height : 0;
 
             float fullHeight = textSize.Height + 2 * padding.Y + 2 * drawStyle.FrameWidth;
@@ -254,21 +250,22 @@ namespace PoeHUD.Hud.Loot
             }
             if (drawStyle.FrameWidth > 0)
             {
-                Graphics.DrawFrame(boxRect, drawStyle.FrameWidth, drawStyle.Color);
+                Graphics.DrawFrame(boxRect, drawStyle.FrameWidth, drawStyle.AlertColor);
             }
             return new Vector2(fullWidth, fullHeight);
         }
 
-        private ItemUsefulProperties EvaluateItem(IEntity item)
+        private ItemUsefulProperties initItem(IEntity item)
         {
             string name = GameController.Files.BaseItemTypes.Translate(item.Path);
-            var usefulProperties = new ItemUsefulProperties(name, item);
-            CraftingBase craftingBase;
-            if (craftingBases.TryGetValue(usefulProperties.Name, out craftingBase) && Settings.Crafting)
+
+            CraftingBase craftingBase = new CraftingBase();
+            if (Settings.Crafting)
             {
-                usefulProperties.SetCraftingBase(craftingBase);
+                craftingBases.TryGetValue(name, out craftingBase);
             }
-            return usefulProperties;
+
+            return new ItemUsefulProperties(name, item, craftingBase);
         }
 
         private string GetItemName(KeyValuePair<EntityWrapper, AlertDrawStyle> kv)
